9b55a32c3876ef83bfe150ca7888bc08a411c073
Giuseppe Pastore <giuseppe.pastore@MacBook-Pro.local>
2019-09-25 17:49:16 +0200

    Updated creaSQL.sql

--- creaDB.sql
+++ creaDB.sql
@@ -5,7 +5,12 @@ USE editor;
 
 
 -- drop tables if they already exist
+DROP TABLE IF EXISTS FILE_USER;
+DROP TABLE IF EXISTS FILE;
 DROP TABLE IF EXISTS USER;
+
+
+
 
 -- create tables
 
@@ -13,6 +18,23 @@ CREATE TABLE USER (
 	Username CHAR(255) ,
 	Nickname CHAR(255) ,
 	Password CHAR(255) NOT NULL,
-	Icon	 CHAR(50),
 	PRIMARY KEY (Username)
+);
+
+CREATE TABLE FILE (
+	Link	CHAR(255),
+	Name  CHAR(255),
+	Owner CHAR(255),
+	Public BOOLEAN,
+	FOREIGN KEY(Owner) REFERENCES USER(Username),
+	PRIMARY KEY(Link)
+);
+
+-- User is not foreign key because the user might be not signed up yet.
+CREATE TABLE FILE_USER (
+	Link CHAR(255),
+	User CHAR(255),
+	First_access BOOLEAN,
+	FOREIGN KEY(Link) REFERENCES FILE(Link),
+	PRIMARY KEY(Link,User)
 );
